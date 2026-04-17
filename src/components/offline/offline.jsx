ae51dcd69976e6a358a06b269e8e4e5e7371a5b6
Hossam Hamad <hsh02928@gmail.com>
2026-04-17 21:58:38 +0200

    fix: casing issues and file paths

--- src/components/offline/offline.jsx
+++ src/components/offline/offline.jsx
@@ -1,9 +1,8 @@
-import { useState } from "react"
-import Style from './Offline.module.css'
-import useIsOnline from "../../Hooks/useIsOnline"
+import { useState } from "react";
+import Style from "./offline.module.css";
+import useIsOnline from "../../Hooks/useIsOnline";
 
 export default function Offline({ children }) {
-    let isOnline = useIsOnline();
-    if (isOnline == false)
-        return children
+  let isOnline = useIsOnline();
+  if (isOnline == false) return children;
 }
